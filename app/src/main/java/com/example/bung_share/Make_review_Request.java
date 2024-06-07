cbd837059e34db200f8f4898423cba94840acb17
배찬호 <1004chanho@gmail.com>
2024-06-07 15:43:55 +0900

    리뷰 올리는거 jsp만 작성해서 올려놨음 데스크탑에서 확인할것

--- app/src/main/java/com/example/bung_share/Make_review_Request.java
+++ app/src/main/java/com/example/bung_share/Make_review_Request.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Make_review_Request extends StringRequest {
-    final static private String URL = "http://27.96.131.54:8080/2020081097/Info.jsp";
+    final static private String URL = "http://27.96.131.54:8080/2020081097/makeReview.jsp";
     private Map<String, String> parameters;
     public Make_review_Request(String userId, String storeId, double rating, String content,Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
